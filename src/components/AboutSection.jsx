33fae329a258276da3119f22d4840780c98cf8b9
Gurivisetti <gopikrishnagurivisetti098@gmail.com>
2025-11-15 09:29:24 +0530

    changed the place of pdf

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -26,7 +26,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/public/Gopi_resume.pdf"
+                href="../assets/Gopi_resume.pdf"
                 download="Gopikrishna_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
